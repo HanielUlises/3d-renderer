dbacf5c236d18b7ff1175d5ff73346df341415ff
HanielUlises <hanielulises2003@gmail.com>
2026-05-20 12:01:23 -0600

    Buffers

---
+++ include/display.h
@@ -0,0 +1,28 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <stdbool.h>
+#include <unistd.h>
+#include <SDL2/SDL.h>
+
+extern uint32_t *color_buffer;
+
+extern int window_width;
+extern int window_height;
+
+extern SDL_Window *window;
+extern SDL_Renderer *renderer;
+extern SDL_Texture *color_buffer_texture;
+
+bool initialize_window(void);
+void setup(void);
+void process_input(void);
+void update(void);
+void render_color_buffer(void);
+void clear_color_buffer(uint32_t color);
+void draw_grid(void);
+void draw_rectangle(int x, int y, int height, int width, uint32_t color);
+void render(void);
+void destroy_window(void);
+
+#endif // DISPLAY_H
