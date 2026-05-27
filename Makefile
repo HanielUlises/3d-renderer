b1f08efe3bed9758d9ac0f4325aacc1d30f13903
HanielUlises <hanielulises2003@gmail.com>
2026-05-27 16:12:33 -0600

    Vector transforms

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 CC = gcc
 CFLAGS = -Wall -std=c99 -I./include `pkg-config --cflags sdl2`
-LIBS = `pkg-config --libs sdl2`
+LIBS = `pkg-config --libs sdl2` -lm
 
 build:
 	$(CC) $(CFLAGS) ./src/*.c -o renderer $(LIBS)
--- src/vector.c
+++ src/vector.c
@@ -1,1 +1,32 @@
+#include <math.h>
 #include "vector.h"
+
+vec3_t vec3_rotate_x(vec3_t v, float angle) {
+    vec3_t rotated_vect = {
+        .x = v.x,
+        .y = v.y * cos(angle) - v.z * sin(angle),
+        .z = v.y * sin(angle) + v.z * cos(angle)
+    };
+
+    return rotated_vect;
+}
+
+vec3_t vec3_rotate_y(vec3_t v, float angle) {
+    vec3_t rotated_vect = {
+        .x = v.x * cos(angle) - v.z * sin(angle),
+        .y = v.y,
+        .z = v.x * sin(angle) + v.z * cos(angle)
+    };
+
+    return rotated_vect;
+}
+
+vec3_t vec3_rotate_z(vec3_t v, float angle) {
+    vec3_t rotated_vect = {
+        .x = v.x * cos(angle) - v.y * sin(angle),
+        .y = v.x * sin(angle) + v.y * cos(angle),
+        .z = v.z
+    };
+
+    return rotated_vect;
+}
